e48c2659f1c39b898cc3a22e5b48a7f9d5efb5bb
Jindrich Vimr <jvimr@softeu.com>
2014-10-08 18:22:01 +0200

    fix

--- run_apache.sh
+++ run_apache.sh
@@ -4,6 +4,9 @@
 set |grep '_PORT_' |tr  '=' ' ' |sed 's/^/SetEnv /' > /etc/apache2/conf-enabled/docker-vars.conf
 set |grep '_ENV_' |tr  '=' ' ' |sed 's/^/SetEnv /' >> /etc/apache2/conf-enabled/docker-vars.conf
 
+echo "export SERVER_NAME=$SERVER_NAME" >> /etc/apache2/envvars
+
+echo "ServerName $SERVER_NAME" > /etc/apache2/conf-enabled/servername.conf
 
 /etc/init.d/apache2 start && \
  tail -F /var/log/apache2/*log
